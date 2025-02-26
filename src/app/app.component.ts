ddf0149b844fdbad8b68aed565dab795b566a285
clondono <calondonoramirez@gmail.com>
2025-02-26 15:58:57 -0500

    Se aplican nuevas capas de modulos compartidos. Se incluyen nuevos modulos Login y sesion. Se incluyen componentes componentes compartidos Layout y Dashboards y se incluye el enrutamiento basico inicial.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DoctorAppFrontend';
+export class AppComponent implements OnInit {
+  title:string = 'DoctorAppFrontendo';
+  users: any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:5275/api/User").subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error), 
+      complete: () => console.log("La solicitud esta completa")
+    });
+  }
 }
